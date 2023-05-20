80d9e181de726dac9f83c6d2594d88fa010196dc
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-05-21 01:45:21 +0300

    Create dossier.py

---
+++ FallenMusic/Helpers/dossier.py
@@ -0,0 +1,96 @@
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from FallenMusic import BOT_NAME
+
+PM_START_TEXT = """ 
+ ‎{0} بەخێر بێیت ئازیزم
+
+**من بۆتێکی سادەم بۆ پەخشکردنی کلیپی دەنگی لە پەیوەندییەکدا**
+‎**و دە توانی بە وشە ی گە ران گورانیە کە ت خێرا تر بدۆزیتە وە**
+‎
+**باشترین بۆت لە ڕووی تایبەتمەندییەوە ‎لەسەر بنەمای کتێبخانە**
+**تیلیتۆن دروستکراوە بۆیە بۆتەکە خێرایە!**
+"""
+
+START_TEXT = """‏
+بەخێر بێیت ئازیزم  {0} ‎‎‏
+
+**من بۆتێکی سادەم بۆ پەخشکردنی کلیپی دەنگی لە پەیوەندییەکدا**
+‎**و دە توانی بە وشە ی گە ران گورانیە کە ت خێرا تر بدۆزیتە وە**
+‎
+**باشترین بۆت لە ڕووی تایبەتمەندییەوە ‎لەسەر بنەمای کتێبخانە**
+**تیلیتۆن دروستکراوە بۆیە بۆتەکە خێرایە!**
+"""
+
+HELP_TEXT = f"""
+<u> **فە رمانە کانی مووزیک کورد {BOT_NAME} :**</u>
+‎• بۆ لێدانی گۆرانییەک لە پەیوەندی دەنگیدا
+‎• بنووسە /play + ناوی گۆرانی
+‎• بۆ ماوەیەکی کاتی پەخشکردنی مۆسیقاکە ڕادەگرێت
+‎• بنوسە /pause
+‎• بۆ دەستپێکردنەوەی ژەنینی مۆسیقا لە پە یوە ندی کە دا
+‎• بنوسە /resume یان لادانی بیدە نگی
+‎• بۆ ئەوەی گۆرانییەکە بەجێبهێڵیت و گۆرانی داهاتوو لێبدەیت
+‎• بنوسە /skip یان /next
+ 
+‎• بۆ کوژاندنەوەی مۆسیقا لە پەیوەندییەکدا
+‎• بنوسە /stop يان /end
+‎• بۆ ئەوەی بزانیت بۆتەکە چەندە خێرا کاردەکات
+‎• بنوسه /ping
+‎• بۆ ئەوەی بزانیت دەربارەی گەشەپێدەرانی بۆت
+‎• بنوسه /sudo
+‎• بۆ گەڕان بەدوای گۆرانییەکدا له يوتيوب
+‎• بنوسه /search
+• بۆ گە رانی خێرا یوزەرنیمی بۆت لە گە ڵ ناوی گۆرانیەکە دا بنووسە 
+• نموونە @MusiccKurdbot حە مە کە رماشانی
+"""
+
+HELP_SUDO = f"""
+<u> **فەرمانەکانی گەشەپێدەر {BOT_NAME} :**</u>
+‎• بۆ بینینی پەیوەندییەکان لە بۆتەکەدا 
+‎• بنوسه /activevc
+‎• بنووسە /eval بۆ ئەوەی لەسەر تێرمیناڵی بۆت کاربکات
+‎• بۆ تاقیکردنەوەی خێرایی سێرڤەرەک
+‎• بنوسه /speedtest يان /spt
+‎• بۆ پیشاندانی ئاماره كاني  سێرڤەر 
+‎• بنوسه /sysstats يان  /stats
+‎• بۆ ناونانی یارمەتیدەرەکە بنوسه /name
+‎• بۆ دۆخی بایۆی یاریدەدەرەکە بنووسە /Bayou
+‎• بۆ گۆرێنی وێنە ی یاریدە رە رە کە بنوسە /setpfp
+‎• بۆ سڕینەوەی وێنەی ئەکاونتی یاریدەدەرەکە بنووسە /delete 
+"""
+
+HELP_DEV = f"""
+<u> **فەرمانی خاوەنی بۆت {BOT_NAME} :**</u>
+‎• ماوسەکە بنووسە بۆ ئەوەی ماوسی دامەزراندنی بۆتەکە بهێنیتە پێشەوە
+‎• بنوسە /config
+‎• ریپلە ی چاتی بکە یان لە گە ڵ فە رمانە کە بینیرە بۆ فوروارد
+‎• بنوسە /forward
+‎• بۆ پاککردنەوەی کاشی بۆتەکە 
+‎• بنوسه /clearcache
+‎• بۆ زیادکردنی گەشەپێدەرێک بۆ بۆتەکە 
+• /addsudo
+‎• بۆ دابەزاندنی گەشەپێدەرەکە لە بۆتەکەوە 
+‎• بنوسه /delsudo
+"""

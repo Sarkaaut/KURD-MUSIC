448d0e817898419f4321a61d45007ff818dfd846
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-05-21 01:25:12 +0300

    Create pause.py

---
+++ FallenMusic/Modules/pause.py
@@ -0,0 +1,48 @@
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from FallenMusic import app, pytgcalls
+from FallenMusic.Helpers import admin_check, close_key, is_streaming, stream_off
+
+
+@app.on_message(filters.command(["pause"]) | filters.command(["Mute","وسبە","وە ستانی کاتی"],prefixes= ["/", "!","","#"]) & filters.group)
+@admin_check
+async def pause_str(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+
+    if not await is_streaming(message.chat.id):
+        return await message.reply_text(
+            "🎵 /resume بۆ تەواوکردنی گۆرانییەکە"
+        )
+
+    await pytgcalls.pause_stream(message.chat.id)
+    await stream_off(message.chat.id)
+    return await message.reply_text(
+        text=f"⏸️ پەخشکردن وەستاوە \n \n🖇️ لە لایە ن : {message.from_user.mention} ",
+        reply_markup=close_key,
+    )

a310a016d5c4221d0c2df200914138100b3eccd6
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-05-21 20:20:44 +0300

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -37,13 +37,14 @@ buttons = InlineKeyboardMarkup(
             InlineKeyboardButton(text="⏸", callback_data="pause_cb"),
             InlineKeyboardButton(text="⏭️", callback_data="skip_cb"),
             InlineKeyboardButton(text="⏹", callback_data="end_cb"),       
-        )
-    ],
-    [
-        InlineKeyboardButton(text="• کە ناڵی بۆت •", url=config.SUPPORT_CHANNEL),
-        InlineKeyboardButton(text="• گروپی بۆت •", url=config.SUPPORT_CHAT),
-    ],
-    [
+            
+            InlineKeyboardButton(text="• کە ناڵی بۆت •", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="• گروپی بۆت •", url=config.SUPPORT_CHAT),
+        ]
+     ]
+)            
+
+
 pm_buttons = [
     [
         InlineKeyboardButton(

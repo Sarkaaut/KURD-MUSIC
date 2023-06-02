9c19537f4f866fbe35c0da8cf317a5d9591d4570
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-06-02 04:27:48 +0300

    Update cleaner.py

--- FallenMusic/Modules/cleaner.py
+++ FallenMusic/Modules/cleaner.py
@@ -46,4 +46,4 @@ async def clear_misc(_, message: Message):
     if os_dir:
         for lel in os.listdir(pth):
             os.system("rm -rf *.webm *.jpg *.png")
-    await message.reply_text("✅ هەمووی پاککرایەوە ")
+    await message.reply_text(" هەمووی پاککرایەوە ✅")

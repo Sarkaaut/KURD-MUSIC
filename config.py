6b8f3951c38f33354bf179637528abb86c8973ba
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-06-03 04:21:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/b18fe3e598295972a774f.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/5cec76ca0466630e0b4db.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/9bf4a8b810c3541d9af7b.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ChanallBots")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5739996356").split()))
 
 
-FAILED = "https://telegra.ph/file/53a98c2a8e16390e35843.jpg"
+FAILED = "https://telegra.ph/file/95bdf306464dbb9cad419.jpg"

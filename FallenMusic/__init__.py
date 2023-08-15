34e3970a834b5db0f3ce4481ff048a81a9c384bf
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-08-15 21:46:15 +0300

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,7 +92,7 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("Gruop_Shetakan")
+        await app2.join_chat("Group_Shetakan")
         await app2.join_chat("gruop_bots")
     except:
         pass
